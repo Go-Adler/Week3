205d1c75ec3869b4c02e1ba4a6bcd26dd5937580
Go-Adler <gokul_adler@outlook.com>
2022-12-20 20:07:16 +0530

    just taking input

--- assignment.js
+++ assignment.js
@@ -1,1 +1,8 @@
-console.log("hello")
+//Assignment Q 1
+
+var something=require ('readline-sync')
+var value = something.question("enter a number")
+var value2 = something.question("enter a number")
+var sum = value + value2
+
+console.log(sum)
